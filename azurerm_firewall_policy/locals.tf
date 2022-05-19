c504e6487fa7613cdca77ddae3931772557cfa00
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-05-19 23:52:22 +0200

    Update 05/19/2022 23:52:22

--- azurerm_firewall_policy/locals.tf
+++ azurerm_firewall_policy/locals.tf
@@ -2,8 +2,8 @@ locals {
   # https://docs.microsoft.com/en-us/azure/firewall/snat-private-range#firewall-policy
 
   rules = {
-    Never       = "0.0.0.0/0"          # Never route/SNAT traffic directly to the Internet
-    Aways       = "255.255.255.255/32" #  Always route/SNAT regardless of the destination address
+    Never       = ["0.0.0.0/0"]          # Never route/SNAT traffic directly to the Internet
+    Aways       = ["255.255.255.255/32"] #  Always route/SNAT regardless of the destination address
     UseIPRanges = var.snat_ip_ranges
   }
   private_ip_ranges = local.rules[local.snat_rules]
