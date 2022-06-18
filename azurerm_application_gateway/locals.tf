12c8c90402030ca15153a1fa9ab52487dacd39a4
Woodworth Wilson <woodworth.wilson@sysco.no>
2022-06-18 15:42:20 +0200

    Update 06/18/2022 15:42:20

--- azurerm_application_gateway/locals.tf
+++ azurerm_application_gateway/locals.tf
@@ -2,7 +2,7 @@
 locals {
   uai_name               = "${var.name}-umi"
   ssl_disabled_protocols = var.ssl_policy.policy_name != null || var.ssl_policy.policy_type != null ? null : var.ssl_policy.disabled_protocols
-  ssl_policy_name        = var.ssl_policy.policy_type == "Predefined" && var.ssl_policy.policy_name == null ? "Default" : var.ssl_policy.name
+  ssl_policy_name        = var.ssl_policy.policy_type == "Predefined" && var.ssl_policy.policy_name == null ? "Default" : var.ssl_policy.policy_name
   is_sku_tier_v2         = can(regex("Standard_v2|WAF_v2", var.tier)) ? true : false
   is_waf                 = can(regex("WAF", var.tier)) ? true : false
 }
